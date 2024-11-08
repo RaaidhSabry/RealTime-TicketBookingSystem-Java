20ebeef2b256b83e31e408fceb7310b2ac2eb907
Raaidh Sabry <raaidh.20232929@iit.ac.lk>
2024-11-09 04:56:07 +0530

    Initial commit

---
+++ src/test/java/com/coursework/ticketbookingsystem/TicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.ticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
